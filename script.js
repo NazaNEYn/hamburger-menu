129f0f53f64cae611cf93ef59bde987b9fdd0b0a
Nazanin Ashrafi <68081983+NazaNEYn@users.noreply.github.com>
2024-06-18 23:06:34 +0330

    Fixed the bug

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const menu = document.querySelector(".list");
 const openHandler = function () {
   menu.classList.remove("hidden");
   menu.classList.add("move-in");
+  menu.classList.remove("move-out");
 };
 
 const closeHandler = function () {
   // menu.classList.add("hidden");
   menu.classList.add("move-out");
+  menu.classList.remove("move-in");
 };
 
 openMenu.addEventListener("click", openHandler);
